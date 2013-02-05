e8cbb21435cf9821cb839ec3d19263fd4738e8a6
Benjamin Guedj <benjamin.guedj@upmc.fr>
2013-02-05 00:00:00 +0000

    version 0.92.2

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(lib, pkg)
 {
-  packageStartupMessage('pacbpred v0.92.1')
+  packageStartupMessage('pacbpred v0.92.2')
   packageStartupMessage('Creator: Benjamin Guedj')
   packageStartupMessage('http://www.lsta.upmc.fr/doct/guedj/index.html')
   packageStartupMessage('If you consider using pacbpred in academic work, please cite it by typing citation("pacbpred").')
